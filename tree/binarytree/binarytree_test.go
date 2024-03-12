fa2aa0f44f839c6da9bc8f05f11dc7c26353b537
Lester <sonorous.lester@gmail.com>
2024-03-12 15:47:32 +0800

    Implement create comelete binary tree

---
+++ tree/binarytree/binarytree_test.go
@@ -0,0 +1,40 @@
+package binarytree
+
+import "testing"
+
+type Node struct {
+	value       int
+	left, right *Node
+}
+
+type Tree struct {
+	root *Node
+}
+
+func LevelOrderBinaryTree(arr []int) *Tree {
+	tree := new(Tree)
+	tree.root = levelOrderBinaryTree(arr, 0, len(arr))
+	return tree
+}
+
+func levelOrderBinaryTree(arr []int, start int, size int) *Node {
+	root := &Node{arr[start], nil, nil}
+
+	left := 2*start + 1
+	right := 2*start + 2
+
+	if left < size {
+		root.left = levelOrderBinaryTree(arr, left, size)
+	}
+
+	if right < size {
+		root.right = levelOrderBinaryTree(arr, right, size)
+	}
+
+	return root
+}
+
+func TestLevelOrderBinaryTree(t *testing.T) {
+	arr := []int{1, 3, 5, 7, 9, 11, 13, 17, 19}
+	levelOrderBinaryTree(arr, 0, len(arr))
+}
